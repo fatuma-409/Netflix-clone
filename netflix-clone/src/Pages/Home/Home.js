42a15d6069b9cad87a2013bd0a458a2bdbcbac78
fatuma-409 <154805810+fatuma-409@users.noreply.github.com>
2024-02-17 11:20:00 -0900

    Initial commit

---
+++ netflix-clone/src/Pages/Home/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../../Components/Header/Header';
+import Footer from '../../Components/Footer/Footer';
+
+const Home = () => {
+  return (
+    <div>
+      <Header />
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
