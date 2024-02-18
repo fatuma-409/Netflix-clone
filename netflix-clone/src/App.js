56292b9ea2152e05120d68cbdf59918a35876c6c
fatuma-409 <154805810+fatuma-409@users.noreply.github.com>
2024-02-17 22:40:46 -0900

    banner

--- netflix-clone/src/App.js
+++ netflix-clone/src/App.js
@@ -1,10 +1,12 @@
-
 import './App.css';
 import Home from './Pages/Home/Home';
+import'../src/index.css'
+
 function App() {
   return (
     <div className="App">
      <Home/>
+    
     </div>
   );
 }
---
+++ netflix-clone/src/Request/Request.js
@@ -0,0 +1,5 @@
+const API_KEY = '6cd12042d59bbfc26b9486c1b0462a27';
+
+const RequestMovies = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}&language=en-US&page=1`;
+
+export default RequestMovies;
